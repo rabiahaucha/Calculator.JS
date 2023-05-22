2d3157eacbb3354b2fc9ac0be7e16922d478b6d7
rabia <rabiahaucha9@gmail.com>
2023-05-22 14:08:16 +0200

    calculator

---
+++ script/code.js
@@ -0,0 +1,52 @@
+// function display(value) {
+//     += value;
+// }
+let sum = "";
+let decimal = [];
+let output = document.querySelector("#output");
+
+function display(symbol) {
+    if(symbol == '.' && decimal.includes('.')){
+        return;
+    } else if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
+        sum = sum + symbol;
+        document.getElementById("output").value = sum;
+        decimal = [];
+
+    } 
+    else {
+        sum = sum + symbol;
+        document.getElementById("output").value = sum;
+        decimal.push(symbol);
+
+    }
+}
+
+// function evaluate() {
+//     // document.getElementById('output').innerText = eval(`${document.getElementById('output').innerText}`).toFixed(2);
+//     output.textContent = output.textContent;
+// }
+
+function backspace() {
+  sum = sum.slice(0, -1);
+  document.getElementById("output").value = sum;
+}
+
+document.querySelector("#equal").addEventListener("click", () => {
+  output.value = eval(output.value).toFixed(2);
+});
+
+document.querySelector('#clear').addEventListener('click', () => {
+    clear()
+})
+
+function clear() {
+    sum = "";
+    document.getElementById('output').innerText = sum;
+
+    output.value = "";
+}
+
+
+
+ 
